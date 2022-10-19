631b42730bca4c9ac094acf077eb6762a7d0e96f
Mike Spade <mikespadeny@gmail.com>
2022-10-19 11:52:55 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+//Take in an array of pokemon and reverse it
+
+function partyReverse(arr){
+  console.log(arr.reverse())
+}
+
+partyReverse(['bulbasaur','charmander','pikach','caterpie','squirtle','zubat'])
+
+//take in 2 arrays 
+
+function squareVsCubed(nums1,nums2){
+  let squared = 0
+  for(nums of nums1){
+    squared += nums * nums
+    console.log(squared)
+  }
+}
